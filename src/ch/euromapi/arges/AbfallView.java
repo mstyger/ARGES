cbfb58f7864f9f602dbf7b46beace46e75ed927a
mstyger <marcel.styger@gmail.com>
2013-11-12 20:11:39 +0100

    add gemeinden and abfalltypes

--- src/ch/euromapi/arges/AbfallView.java
+++ src/ch/euromapi/arges/AbfallView.java
@@ -1,7 +1,6 @@
 package ch.euromapi.arges;
 
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -9,19 +8,20 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends ListActivity {
+public class AbfallView extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.abfall_view);
 
 		// storing string resources into Array
-		String[] gemeinden = getResources().getStringArray(R.array.gemeinden);
+		String[] abfalltypen = getResources().getStringArray(
+				R.array.abfalltypen);
 
 		// Binding resources Array to ListAdapter
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, gemeinden);
+				android.R.layout.simple_list_item_1, abfalltypen);
 
 		// Assign adapter to List
 		setListAdapter(adapter);
@@ -40,19 +40,7 @@ public class MainActivity extends ListActivity {
 
 		Toast.makeText(getApplicationContext(), itemPosition + " " + itemValue,
 				Toast.LENGTH_LONG).show();
-		
-		Intent intent = new Intent(this, AbfallView.class);
-	    intent.putExtra("gemeinde_pos", itemPosition);
-	    intent.putExtra("gemeinde_name", itemValue);
-	    startActivity(intent);
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 }
