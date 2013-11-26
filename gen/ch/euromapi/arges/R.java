36adef407f8e03db0347b05cf34d48b08bbb8f8e
mstyger <marcel.styger@gmail.com>
2013-11-26 20:55:30 +0100

    remove setting tab

--- gen/ch/euromapi/arges/R.java
+++ gen/ch/euromapi/arges/R.java
@@ -29,20 +29,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090001;
+        public static final int webview=0x7f090000;
     }
     public static final class layout {
         public static final int abfall_view=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int detail_view=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int abfall_title=0x7f060001;
-        public static final int action_settings=0x7f060002;
+        public static final int abfall_title=0x7f060002;
+        public static final int action_settings=0x7f060003;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060003;
+        public static final int details_title=0x7f060001;
+        public static final int hello_world=0x7f060004;
     }
     public static final class style {
         /** 
--- src/ch/euromapi/arges/MainActivity.java
+++ src/ch/euromapi/arges/MainActivity.java
@@ -3,11 +3,9 @@ package ch.euromapi.arges;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
@@ -42,13 +40,6 @@ public class MainActivity extends ListActivity {
 	    intent.putExtra("gemeinde", itemValue);
 	    startActivity(intent);
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 }
